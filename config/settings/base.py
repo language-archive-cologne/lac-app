415653fa8a458d0e3e25cb2e4129d4b9fe55bd67
Francisco Mondaca <mondaca@uni-koeln.de>
2025-03-09 14:29:48 +0100

    storage init

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,6 +87,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "lacos.users",
     "lacos.blam",
+    "lacos.storage",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
