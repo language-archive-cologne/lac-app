0fd734c13830aa27d8bb3d387165308e0c69e0ec
Francisco Mondaca <mondaca@uni-koeln.de>
2025-03-13 13:52:26 +0100

    s3 added

--- lacos/blam/models/__init__.py
+++ lacos/blam/models/__init__.py
@@ -1,6 +1,4 @@
 from .bundle import *
 from .base_model import *
-from .base_resources import *
-from .base_identifiers import *
 from .base_repository import *
 from .base_administrative_info import *
--- lacos/blam/models/base_repository.py
+++ lacos/blam/models/base_repository.py
@@ -1,5 +1,6 @@
 from django.db import models
 from lacos.blam.models.base_model import BaseModel       
+from lacos.storage.models import S3ResourceLocation, ACFLPermissions
 
 class Repository(BaseModel):
     """
@@ -49,3 +50,78 @@ class Repository(BaseModel):
     
     class Meta:
         abstract = True
+
+    def get_s3_location(self):
+        """Get the S3 location for this repository"""
+        try:
+            from django.contrib.contenttypes.models import ContentType
+            ct = ContentType.objects.get_for_model(self)
+            location = S3ResourceLocation.objects.get(
+                content_type=ct,
+                object_id=self.id
+            )
+            return location
+        except S3ResourceLocation.DoesNotExist:
+            return None
+
+    def get_s3_url(self):
+        """Get the S3 URL for this repository"""
+        location = self.get_s3_location()
+        if location:
+            return location.get_s3_url()
+        return None
+        
+    def get_presigned_url(self, expires_in=3600):
+        """Generate a presigned URL for temporary access"""
+        location = self.get_s3_location()
+        if location:
+            from lacos.storage.services import S3Service
+            return S3Service.generate_presigned_url(
+                location.s3_bucket,
+                location.s3_key,
+                expires_in=expires_in
+            )
+        return None
+
+    def get_acfl_permissions(self):
+        """Get the ACFL permissions for this repository"""
+        try:
+            from django.contrib.contenttypes.models import ContentType
+            ct = ContentType.objects.get_for_model(self)
+            permissions = ACFLPermissions.objects.get(
+                content_type=ct,
+                object_id=self.id
+            )
+            return permissions
+        except ACFLPermissions.DoesNotExist:
+            return None
+            
+    def refresh_acfl_permissions(self):
+        """Refresh ACFL permissions from S3"""
+        from lacos.storage.services import ACFLService
+        return ACFLService.refresh_permissions(self)
+        
+    def check_permission(self, user, permission_type='read'):
+        """Check if a user has permission on this repository"""
+        from lacos.storage.services import ACFLService
+        return ACFLService.check_permission(user, self, permission_type)
+
+    def register_s3_location(self, bucket, key, pid_url=None):
+        """Register S3 location for this repository"""
+        from lacos.storage.services import S3Service
+        return S3Service.register_s3_location(self, bucket, key, pid_url)
+        
+    def register_acfl_file(self, bucket, key):
+        """Register ACFL file location for this repository"""
+        from django.contrib.contenttypes.models import ContentType
+        ct = ContentType.objects.get_for_model(self)
+        
+        acfl, created = ACFLPermissions.objects.update_or_create(
+            content_type=ct,
+            object_id=self.id,
+            defaults={
+                'acfl_file_bucket': bucket,
+                'acfl_file_key': key
+            }
+        )
+        return acfl
