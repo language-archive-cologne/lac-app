c76103a63338364e6fccdc6eda280bedc35b6868
Francisco Mondaca <mondaca@uni-koeln.de>
2025-12-05 17:18:22 +0100

    fix: update services __init__ for renamed acl module

--- lacos/storage/services/__init__.py
+++ lacos/storage/services/__init__.py
@@ -1,9 +1,10 @@
 from .acl_evaluation_service import ACLEvaluationService
-from .acl_sync_service import ACLSyncService
+from .acl_service import ACLService, ACLSyncService  # ACLSyncService is backwards compat alias
 from .resource_mapping_service import ACFLService, ResourceMappingService, S3Service
 
 __all__ = [
-    "ACLSyncService",
+    "ACLService",
+    "ACLSyncService",  # backwards compat
     "ACLEvaluationService",
     "ResourceMappingService",
     "S3Service",
